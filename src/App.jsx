10b600fce2d6ff8df6e42c468a834713f1516410
Beza-stack <bezawitsolomon635@gmail.com>
2025-02-17 03:14:38 +0400

    Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Components/Header/Header'
+import Carousel  from './Components/Carousel/Carousel';
 
 function App() {
   return (
     <div>
-        <Header />
+      <Header />
+      <Carousel />
     </div>
-  )
+  );
 }
 
 export default App
